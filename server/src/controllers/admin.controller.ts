b424c10dfe8df150995d8503e568805d27b07c63
cakezero <sareahj.com@gmail.com>
2026-01-20 18:34:08 +0100

    admin actions

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -1,7 +1,13 @@
+import bcrypt from "bcrypt";
 import logger from "@/config/logger";
 import { quest } from "@/models/quests.model";
-import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from "@/utils/status.utils";
+import { admin } from "@/models/admin.model";
+import crypto from "crypto";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, OK, UNAUTHORIZED } from "@/utils/status.utils";
 import { validateQuestData } from "@/utils/utils";
+import { sendEmailToAdmin } from "@/utils/sendMail";
+import { campaignQuestCompleted, miniQuestCompleted } from "@/models/questsCompleted.models";
+import { submission } from "@/models/submission.model";
 
 export const createQuest = async (req: GlobalRequest, res: GlobalResponse) => {
 	try {
@@ -17,7 +23,7 @@ export const createQuest = async (req: GlobalRequest, res: GlobalResponse) => {
 		if (newQuest.category !== "weekly") {
 			await newQuest.save();
 		} else {
-			// new Date(new Date(Date.now() + 86400000).setHours(0, 60, 0, 0)); (if quests expire at 12am UTC. For now, it expires 1 day after creation)
+			// new Date(new Date(Date.now() + 86400000).setHours(0, 60, 0, 0)); (quests expire at 12am UTC. For now, it expires 1 day after creation)
 			newQuest.expires = new Date(Date.now() + 86400000);
 			await newQuest.save();
 		}
@@ -28,3 +34,201 @@ export const createQuest = async (req: GlobalRequest, res: GlobalResponse) => {
 		res.status(INTERNAL_SERVER_ERROR).json({ error: "error creating quest" });
 	}
 };
+
+export const getAdmins = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const admins = await admin.find();
+
+		res.status(OK).json({ message: "admins fetched", admins });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error fetching admins" });
+	}
+}
+
+export const removeAdmin = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const { email }: { email: string } = req.body;
+		if (!email) {
+			res.status(BAD_REQUEST).json({ error: "send admin email" });
+			return;
+		}
+
+		const adminExists = await admin.findOne({ email });
+		if (!adminExists) {
+			res.status(NOT_FOUND).json({ error: "admin does not exist" });
+			return;
+		}
+
+		await admin.findOneAndDelete({ email });
+
+		res.status(OK).json({ message: "admin removed" });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error removing admin" });
+	}
+}
+
+export const addAdmin = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const { email }: { email: string } = req.body;
+		if (!email) {
+			res.status(BAD_REQUEST).json({ error: "send admin email" });
+			return;
+		}
+
+		const emailExists = await admin.findOne({ email });
+		if (!emailExists) {
+			const newAdmin = new admin(req.body);
+
+			const code = crypto
+				.randomInt(0, 1000000000)
+				.toString()
+				.padStart(6, "0");
+
+			newAdmin.code = code;
+
+			await sendEmailToAdmin(email, code);
+
+			await newAdmin.save();
+
+			res.status(OK).json({ message: "created" });
+			return;
+		}
+
+		res.status(BAD_REQUEST).json({ error: "admin with email exists" });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error adding admin" });
+	}
+}
+
+export const adminLogin = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const { email, password }: { email: string; password: string } = req.body;
+
+		const adminExists = await admin.findOne({ email });
+		if (!adminExists) {
+			res.status(BAD_REQUEST).json({ error: "invalid credentials" });
+			return;
+		}
+
+		const passwordCorrect = await bcrypt.compare(password, adminExists.password ?? "");
+		if (!passwordCorrect) {
+			res.status(BAD_REQUEST).json({ error: "invalid credentials" });
+			return;
+		}
+
+		res.status(OK).json({ message: "admin logged in" });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error fetching tasks" });
+	}
+};
+
+export const createAdmin = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const { password, confirmPassword, email, code }: { password: string; confirmPassword: string; code: string; email: string } = req.body;
+
+		if (!password || !confirmPassword || !email) {
+			res.status(BAD_REQUEST).json({ error: "send the required details" });
+			return;
+		}
+
+		if (password !== confirmPassword) {
+			res.status(BAD_REQUEST).json({ error: "passwords do not match" });
+			return;
+		}
+
+		const semiAdmin = await admin.findOne({ email, verified: false });
+		if (!semiAdmin) {
+			res.status(NOT_FOUND).json({ error: "email is invalid or admin has been verified. contact master admin if issue persists" });
+			return;
+		}
+
+		if (code !== semiAdmin.code) {
+			res.status(UNAUTHORIZED).json({ error: "code sent is invalid" });
+			return;
+		}
+
+		const salt = bcrypt.genSaltSync(12);
+
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		semiAdmin.verified = true;
+		semiAdmin.password = hashedPassword;
+		semiAdmin.code = "code";
+
+		await semiAdmin.save();
+
+		res.status(OK).json({ message: "admin verified" });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error fetching tasks" });
+	}
+};
+
+export const getTasks = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const pendingTasks = await submission.find();
+		
+		res.status(OK).json({ message: "submitted tasks fetched", pendingTasks });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error fetching tasks" });
+	}
+};
+
+export const markTask = async (req: GlobalRequest, res: GlobalResponse) => {
+	try {
+		const { id, action } = req.query as { id: string; action: string };
+
+		const taskToBeVerified = await submission.findById(id);
+
+		if (!taskToBeVerified) {
+			res.status(NOT_FOUND).json({ error: "task does not exist" });
+			return
+		}
+
+		let model;
+
+		if (taskToBeVerified.page === "quest") {
+			model = await miniQuestCompleted.findOne({ _id: taskToBeVerified.questCompleted, status: { $in: ["pending", "retry"] } });
+			if (!model) {
+				res.status(NOT_FOUND).json({ error: "mini quest already completed or is invalid" });
+				return
+			}
+
+			if (action !== "done") {
+				taskToBeVerified.status = "done";
+				model.status = "done"
+			} else {
+				taskToBeVerified.status = "retry";
+				model.status = "retry";
+			}
+
+		} else {
+			model = await campaignQuestCompleted.findOne({ _id: taskToBeVerified.questCompleted, status: { $in: ["pending", "retry"] } });
+			if (!model) {
+				res.status(NOT_FOUND).json({ error: "campaign quest already completed or is invalid" });
+				return
+			}
+
+			if (action !== "done") {
+				taskToBeVerified.status = "done";
+				model.status = "done"
+			} else {
+				taskToBeVerified.status = "retry";
+				model.status = "retry";
+			}
+		}
+
+		await taskToBeVerified.save();
+		await model.save();
+
+		res.status(OK).json({ message: "task marked" });
+	} catch (error) {
+		logger.error(error);
+		res.status(INTERNAL_SERVER_ERROR).json({ error: "error marking task" });
+	}
+}
