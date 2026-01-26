f1aede0b01c9b0976d082932503d70827cec48df
cakezero <sareahj.com@gmail.com>
2026-01-26 16:20:24 +0100

    test ratelimter

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import AnimatedBackground from "../components/AnimatedBackground"
 import { Link, useLocation } from "wouter";
 import { motion } from "framer-motion";
-import React from "react";
+import { useEffect } from "react";
+import { apiRequestV2 } from "../lib/queryClient";
 
 export default function Home() {
+  useEffect(() => {
+    (async () => await apiRequestV2("/api/"))()
+  })
+
   return (
     <>
     <div className="relative w-full min-h-screen overflow-x-hidden">
